2254bb86b7dc0335a96cffc3271dba6beacf3235
one-color-low <gakki.uec15@gmail.com>
2023-05-21 22:43:35 +0900

    message to VRMPose変換成功

--- osc_client.py
+++ osc_client.py
@@ -1,4 +1,5 @@
 from pythonosc import udp_client
+import time
 
 # OSCサーバーのアドレスとポートに合わせて設定
 osc_server_ip = '0.0.0.0'
@@ -8,6 +9,24 @@ osc_server_port = 8000
 client = udp_client.SimpleUDPClient(osc_server_ip, osc_server_port)
 
 # OSCメッセージを送信する例
-address = '/example/address'
-args = [1, 2, 3.14, 'hello']
+address = '/VMC/Ext/Root/Pos'
+args = ['root', -0.037, -0.090, 0.596, 0.000, 0.000, 0.000, 1.000]
+client.send_message(address, args)
+
+time.sleep(1)
+
+address = '/VMC/Ext/Bone/Pos'
+args = ['leftUpperLeg', -0.037, -0.090, 0.596, 0.000, 0.000, 0.000, 1.000]
+client.send_message(address, args)
+
+time.sleep(1)
+
+address = '/VMC/Ext/Bone/Pos'
+args = ['leftLowerLeg', -0.037, -0.090, 0.596, 0.000, 0.000, 0.000, 1.000]
+client.send_message(address, args)
+
+time.sleep(1)
+
+address = '/VMC/Ext/Root/Pos'
+args = ['root', -0.037, -0.090, 0.596, 0.000, 0.000, 0.000, 1.000]
 client.send_message(address, args)
